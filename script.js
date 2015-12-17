e38ad7f84c9bb16cc911f2dc84d45316bd1c46b0
Juliette <jcougnoux@gmail.com>
2015-12-17 06:57:49 +0100

    ajouter du compteur de points

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ function newDestination() {
   return Point.random() * view.size;
 }
 
+var score = 0;
+
+var text = new PointText(new Point(10, 20));
+text.fillColor = 'black';
+
+text.content = score;
+
+function points() {
+   return score ++;
+}
+
 function createCircle() {
   var c = new Path.Circle({
     center: Point.random() * view.size,
@@ -24,19 +35,20 @@ function createCircle() {
 
   c.onClick = function(event) {
     this.remove();
-
     circ = createCircle();
     destination = newDestination();
+    text.content = points();
   }
   return c;
-
 }
+
 var circ = createCircle();
 var destination = newDestination();
 
+
 function onFrameBall(event) {
   var vector = destination - circ.position;
-  circ.position += vector / 20;
+  circ.position += vector / 13;
   circ.content = Math.random(vector.isZero);
   if (vector.length < 5) {
     destination = newDestination();
