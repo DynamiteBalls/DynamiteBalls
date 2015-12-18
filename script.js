29673e7525292af9bd696db00fea4e3526d65a68
tonino24 <anthony.perez_24@live.fr>
2015-12-18 14:00:32 +0100

    the final countdown

--- script.js
+++ script.js
@@ -1,22 +1,60 @@
+// Appel des fonction Onframe
+
 function onFrame(event) {
   onFrameBall(event);
 };
 
+// Fonction destination
+
 function newDestination() {
   return Point.random() * view.size;
-}
+};
+
+// Fonctionnalité score
 
 var score = 0;
 
 var text = new PointText(new Point(10, 30));
 text.fillColor = 'red';
 text.fontFamily = 'Cursive',
-text.fontSize = 30,
-text.content = score;
+  text.fontSize = 30,
+  text.content = score;
 
 function points() {
-   return score ++;
+  return score++;
+};
+
+// Fonctionnalité chrono
+
+var chrono = 60;
+
+var started = false;
+
+view.onMouseUp = function() {
+  started = true;
 }
+
+function updateChrono() {
+  if (started === true) {
+    text2.content = chrono--;
+  }
+}
+
+function end() {
+  if (chrono === 0) {
+    return 'GAME OVER Votre score est : ' + score;
+  }
+}
+
+setInterval(updateChrono, 1000);
+
+var text2 = new PointText(new Point(470, 30));
+text2.fillColor = 'red';
+text2.fontFamily = 'Cursive';
+text2.fontSize = 30;
+text2.content = chrono;
+
+// Fonction cercles (création suppression et mouvement)
 
 function createCircle() {
   var c = new Path.Circle({
@@ -46,11 +84,9 @@ function createCircle() {
 var circ = createCircle();
 var destination = newDestination();
 
-
 function onFrameBall(event) {
   var vector = destination - circ.position;
   circ.position += vector / 10;
-  circ.content = Math.random(vector.isZero);
   if (vector.length < 5) {
     destination = newDestination();
   }
